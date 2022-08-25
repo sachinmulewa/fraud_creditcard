a1603a79c9fc1e593bac89c76eb97e4b922797df
sachinmulewa <109524278+sachinmulewa@users.noreply.github.com>
2022-08-25 19:06:51 +0530

    Add files via upload

--- fraud.py
+++ fraud.py
@@ -21,6 +21,8 @@ print(X.info())
 print(y.info())
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 X_train,X_test,y_train,y_test=train_test_split(X,y,train_size= 0.8)
 log=LogisticRegression ()
 log.fit(X_train ,y_train )
@@ -30,9 +32,30 @@ print(y_pred)
 #print(y_pred)
 y_pred=pd.DataFrame (y_pred)
 y_pred.columns=["predicted"]
+tree= DecisionTreeClassifier ()
+tree.fit(X_train ,y_train )
+y_pred_tree=tree.predict(X_test)
+print(y_pred_tree)
+#y_pred.columns=["predicted"]
+#print(y_pred)
+y_pred_tree=pd.DataFrame (y_pred_tree)
+y_pred_tree.columns=["predicted"]
+rftree= RandomForestClassifier  ()
+rftree.fit(X_train ,y_train )
+y_pred_rftree=rftree.predict(X_test)
+print(y_pred_rftree)
+#y_pred.columns=["predicted"]
+#print(y_pred)
+y_pred_rftree=pd.DataFrame (y_pred_rftree)
+y_pred_rftree.columns=["predicted"]
 from sklearn .metrics import accuracy_score
 accuracy=accuracy_score(y_test,y_pred )
 print("accuracy=",accuracy )
+accuracy_tree=accuracy_score(y_test,y_pred_tree )
+print("accuracy=",accuracy_tree)
+accuracy_rftree=accuracy_score(y_test,y_pred_rftree )
+print("accuracy=",accuracy_rftree)
+
 
 
 
